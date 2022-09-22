aa15b599811b41f020304da4cd0faaed95c2f96c
Lokesh Gautham B M <56081429+loki-777@users.noreply.github.com>
2022-09-23 00:29:59 +0530

    Modified train.

--- main/train.py
+++ main/train.py
@@ -4,13 +4,16 @@ from torch import nn
 from torch.nn import functional as F
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.loggers import WandbLogger
 import monai
 from Models import JointModel
 from utils import LinearWarmupCosineAnnealingLR, load_config, save_model
 import data_setup
 import os
 import datetime
-import wandb, sys
+import sys
+import wandb
+import GPUtil
 
 # SETTING UP CONFIGS
 
@@ -18,16 +21,7 @@ CONFIG_PATH = "configs/"
 config_name = sys.argv[1]
 
 config = load_config(CONFIG_PATH, config_name)
-
-
-# WANDB INIT
-
-wandb.init(
-    project=config["wandb"]["project"],
-    config=config["wandb"]["config"],
-    name= config["wandb"]["run_name"] + "-" + str(datetime.datetime.now())
-)
-
+wandb_logger = WandbLogger(name=config["wandb"]["run_name"] + "-" + str(datetime.datetime.now()), project=config["wandb"]["project"], log_model="all")
 
 # LIGHTNING MODEL
 class JointModelLightning(pl.LightningModule):
@@ -50,12 +44,10 @@ class JointModelLightning(pl.LightningModule):
 
         NUM_EPOCHS,
         LEARNING_RATE,
-        WARMUP_EPOCHS,
-        save,
-        every_n_epoch
+        WARMUP_EPOCHS
         ):
         super().__init__()
-        
+
         self.model = JointModel(in_channels=in_channels,
             img_size=img_size,
             patch_size=patch_size,
@@ -67,18 +59,11 @@ class JointModelLightning(pl.LightningModule):
         self.NUM_EPOCHS = NUM_EPOCHS
         self.LEARNING_RATE = LEARNING_RATE
         self.WARMUP_EPOCHS = WARMUP_EPOCHS
-        self.LOSS_FN = monai.losses.DiceCELoss(include_background=False, smooth_nr=0, smooth_dr=1e-6)
+        self.LOSS_FN = monai.losses.DiceCELoss(smooth_nr=0, smooth_dr=1e-6)
         self.OPTIMIZER = torch.optim.AdamW(self.model.parameters(), lr=self.LEARNING_RATE)
         self.SCHEDULER = LinearWarmupCosineAnnealingLR(self.OPTIMIZER, warmup_epochs=self.WARMUP_EPOCHS, max_epochs=self.NUM_EPOCHS)
         self.dice_metric = monai.metrics.DiceMetric()
-
-        self.global_min_validation_loss = 1000000
-        self.global_max_validation_acc = 0
-        self.save = save
-        self.every_n_epoch = every_n_epoch
-        self.best_val_loss_epoch = 0
-        self.best_val_acc_epoch = 0
-        self.save_hyperparameters()
+	self.save_hyperparameters()
 
     def forward(self, X):
         return self.model(X)
@@ -86,7 +71,7 @@ class JointModelLightning(pl.LightningModule):
     def configure_optimizers(self):
         optimizer = torch.optim.AdamW(self.parameters(), lr=self.LEARNING_RATE)
         return optimizer
-    
+
     def training_step(self, batch, batch_idx):
         X, y = batch
         X = X.type(torch.cuda.FloatTensor)
@@ -94,8 +79,11 @@ class JointModelLightning(pl.LightningModule):
         segmentation_pred, recon_score, recon_pred = self.model(X)
         segmentation_loss = self.LOSS_FN(segmentation_pred, y)
         loss = segmentation_loss + self.LAMBDA*recon_score
-        return {"loss": loss, "train_segmentation_loss": segmentation_loss, "train_reconstruction_loss": recon_score}
-    
+        self.log("loss", loss, sync_dist=True)
+        self.log("segmentation_loss", segmentation_loss, sync_dist=True)
+        self.log("reconstruction_loss", recon_score, sync_dist=True)
+        return loss
+
     def validation_step(self, batch, batch_idx):
         X, y = batch
         X = X.type(torch.cuda.FloatTensor)
@@ -103,44 +91,20 @@ class JointModelLightning(pl.LightningModule):
         segmentation_pred, recon_score, recon_pred = self.model(X)
         segmentation_loss = self.LOSS_FN(segmentation_pred, y)
         val_loss = segmentation_loss + self.LAMBDA*recon_score
+        segmentation_pred = (segmentation_pred>0.5)
+        y = y > 0.5
         self.dice_metric(y_pred=segmentation_pred, y=y)
         val_segmentation_accuracy = self.dice_metric.aggregate().item()
         self.dice_metric.reset()
+        self.log("val_loss", val_loss, sync_dist=True)
+        self.log("val_segmentation_loss", segmentation_loss, sync_dist=True)
+        self.log("val_reconstruction_loss", recon_score, sync_dist=True)
+        self.log("val_acc", val_segmentation_accuracy, sync_dist=True)
+        return val_loss
 
-        return {"val_loss": val_loss, "val_segmentation_loss": segmentation_loss, "val_reconstruction_loss": recon_score, "val_segmentation_accuracy": val_segmentation_accuracy}
-    
-    def training_epoch_end(self, outputs) -> None:
-        gathered = self.all_gather(outputs)
-        if self.global_rank == 0:
-            train_loss = sum(output['loss'].mean() for output in gathered) / len(outputs)
-            train_segmentation_loss = sum(output['train_segmentation_loss'].mean() for output in gathered) / len(outputs)
-            train_reconstruction_loss = sum(output['train_reconstruction_loss'].mean() for output in gathered) / len(outputs)
-            print(f"Epoch: {self.current_epoch} | loss: {train_loss.item()} | train_segmentation_loss: {train_segmentation_loss.item()} | train_reconstruction_loss: {train_reconstruction_loss.item()}")
-            wandb.log({"train_epoch": self.current_epoch, "train_loss": train_loss, "train_segmentation_loss": train_segmentation_loss, "train_reconstruction_loss": train_reconstruction_loss})
 
-    def validation_epoch_end(self, outputs) -> None:
-        gathered = self.all_gather(outputs)
-        if self.global_rank == 0:
-            val_loss = sum(output['val_loss'].mean() for output in gathered) / len(outputs)
-            val_segmentation_loss = sum(output['val_segmentation_loss'].mean() for output in gathered) / len(outputs)
-            val_reconstruction_loss = sum(output['val_reconstruction_loss'].mean() for output in gathered) / len(outputs)
-            val_segmentation_accuracy = sum(output['val_segmentation_accuracy'].mean() for output in gathered) / len(outputs)
-            if(self.current_epoch % self.every_n_epoch == 0):
-                if(self.save == True):
-                    save_model(self.model, "saved_models/checkpoints", f"epoch-{self.current_epoch}.pth")
-            if(val_loss < self.global_min_validation_loss):
-                save_model(self.model, "saved_models", "best_val_loss.pth")
-                self.global_min_validation_loss = val_loss
-                self.best_val_loss_epoch = self.current_epoch
 
-            if(val_segmentation_accuracy > self.global_max_validation_acc):
-                save_model(self.model, "saved_models", "best_val_acc.pth")
-                self.global_max_validation_acc = val_segmentation_accuracy
-                self.best_val_acc_epoch = self.current_epoch
-            
-            print(f"Epoch: {self.current_epoch} | val_loss: {val_loss.item()} |  val_segmentation_loss: {val_segmentation_loss.item()} |  val_reconstruction_loss: {val_reconstruction_loss.item()}")
-            wandb.log({"val_epoch": self.current_epoch, "val_loss": val_loss, "val_segmentation_loss": val_segmentation_loss, "val_reconstruction_loss": val_reconstruction_loss, "val_segmentation_accuracy": val_segmentation_accuracy})
-
+NO_GPUS = torch.cuda.device_count()
 
 
 # DEFINE DATALOADERS
@@ -148,8 +112,9 @@ class JointModelLightning(pl.LightningModule):
 train_dataloader, test_dataloader, val_dataloader = data_setup.create_dataloaders(
         dataset= os.path.join("../data", config["dataset_name"]),
         batch_size=config["training_parameters"]["batch_size"],
-        img_size=(config["model_parameters"]["img_size_w"], config["model_parameters"]["img_size_h"])
-    )
+        img_size=(config["model_parameters"]["img_size_w"], config["model_parameters"]["img_size_h"]),
+        num_workers=4*NO_GPUS,
+        augmentation=config["training_parameters"]["augmentation"])
 
 
 # INITIALISE MODEL
@@ -164,27 +129,38 @@ model = JointModelLightning(
     LAMBDA=config["training_parameters"]["lambda"],
     NUM_EPOCHS=config["training_parameters"]["num_epochs"],
     LEARNING_RATE=config["training_parameters"]["learning_rate"],
-    WARMUP_EPOCHS=config["training_parameters"]["warmup_epochs"],
-    save=config["training_parameters"]["save"],
-    every_n_epoch=config["training_parameters"]["every_n_epoch"]
-    )
+    WARMUP_EPOCHS=config["training_parameters"]["warmup_epochs"])
 
-# TRAINING
 
-NO_GPUS = torch.cuda.device_count()
-resume = config["training_parameters"]["resume_training"]
 
-# checkpoint_callback = ModelCheckpoint(dirpath="./checkpoints",save_last=True, every_n_epochs=config["save_model"]["every_n_epoch"], save_on_train_epoch_end = True, filename='{epoch}-{train_loss:.4f}')
+overall_checkpoint_callback = ModelCheckpoint(
+        dirpath="saved_models/checkpoints/",
+        filename="{epoch}",
+        monitor="loss",
+        save_top_k=-1,
+        mode="min")
 
-if(resume == True):
-    trainer = pl.Trainer(devices=NO_GPUS, accelerator="gpu", strategy="ddp", 
-    max_epochs=config["training_parameters"]["num_epochs"], resume_from_checkpoint='./checkpoints/last.ckpt')
-else:
-    trainer = pl.Trainer(devices=NO_GPUS, accelerator="gpu", strategy="ddp", 
-    max_epochs=config["training_parameters"]["num_epochs"])
+val_loss_checkpoint_callback = ModelCheckpoint(
+        dirpath="saved_models",
+        filename="best_val_loss-{epoch}",
+        monitor="val_loss",
+        save_top_k=1,
+        mode="min")
+
+val_acc_checkpoint_callback = ModelCheckpoint(
+        dirpath="saved_models",
+        filename="best_val_acc-{epoch}",
+        monitor="val_acc",
+        save_top_k=1,
+        mode="max")
+
+
+trainer = pl.Trainer(devices=NO_GPUS, 
+        accelerator="gpu", 
+        strategy="ddp", 
+        logger=wandb_logger, 
+        callbacks=[overall_checkpoint_callback, val_loss_checkpoint_callback, val_acc_checkpoint_callback], 
+        max_epochs=config["training_parameters"]["num_epochs"])
 
 trainer.fit(model=model, train_dataloaders=train_dataloader, val_dataloaders=val_dataloader)
 
-print(f"BEST VAL ACC EPOCH: {model.best_val_acc_epoch} || BEST VAL LOSS EPOCH: {model.best_val_loss_epoch}")
-
-wandb.finish()
