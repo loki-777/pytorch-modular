82ee279db483d87526db24475be0fcab05e735a4
Lokesh Gautham B M <56081429+loki-777@users.noreply.github.com>
2022-09-27 15:42:38 +0530

    updated data_augmentaion

--- data/data_augmentation.py
+++ data/data_augmentation.py
@@ -1,6 +1,10 @@
 import os
 import cv2
 import random
+import numpy as np
+import torch
+import PIL
+import torchvision
 
 def create_augmentations(input_dir, mask_dir,  crop_size, transforms=[]):
     input_imgs = os.listdir(input_dir)
@@ -9,13 +13,13 @@ def create_augmentations(input_dir, mask_dir,  crop_size, transforms=[]):
         mask = cv2.imread(os.path.join(mask_dir, img_path), 0)
         for t in transforms:
             if(t=='flip'):
-                img_t = cv2.flip(img)
-                mask_t = cv2.flip(mask)
+                img_t = cv2.flip(img, 0)
+                mask_t = cv2.flip(mask, 0)
                 if(not os.path.exists("./flip/")):
-                    os.mkdir("./flip/images/")
-                    os.mkdir("./flip/masks/")
-                cv2.imwrite(f'./flip/images/{img_path[:-3]}_f.jpg', img_t)
-                cv2.imwrite(f'./flip/mask/{img_path[:-3]}_f.jpg', mask_t)
+                    os.makedirs("./flip/images/")
+                    os.makedirs("./flip/masks/")
+                cv2.imwrite(f'./flip/images/{img_path[:-4]}_f.jpg', img_t)
+                cv2.imwrite(f'./flip/masks/{img_path[:-4]}_f.jpg', mask_t)
             elif(t=='rotate'):
                 angle = random.randrange(-10,11)
                 h,w = img.shape
@@ -24,18 +28,41 @@ def create_augmentations(input_dir, mask_dir,  crop_size, transforms=[]):
                 img_t = cv2.warpAffine(src=img, M=rot_matrix, dsize=(w,h))
                 mask_t = cv2.warpAffine(src=mask, M=rot_matrix, dsize=(w,h))
                 if(not os.path.exists("./rotate/")):
-                    os.mkdir("./rotate/images/")
-                    os.mkdir("./rotate/masks/")
-                cv2.imwrite(f'./rotate/images/{img_path[:-3]}_r.jpg', img_t)
-                cv2.imwrite(f'./rotate/mask/{img_path[:-3]}_r.jpg', mask_t)
+                    os.makedirs("./rotate/images/")
+                    os.makedirs("./rotate/masks/")
+                cv2.imwrite(f'./rotate/images/{img_path[:-4]}_r.jpg', img_t)
+                cv2.imwrite(f'./rotate/masks/{img_path[:-4]}_r.jpg', mask_t)
             elif(t=='crop'):
                 h,w = img.shape
-                img_t = img_t[h/2 - crop_size[0]/2: h/2 + crop_size[0]/2][w/2 - crop_size[1]/2 : w/2 + crop_size[1]/2]
-                mask_t = mask_t[h/2 - crop_size[0]/2: h/2 + crop_size[0]/2][w/2 - crop_size[1]/2 : w/2 + crop_size[1]/2]
+                #img_t = img_t[h/2 - crop_size[0]/2: h/2 + crop_size[0]/2][w/2 - crop_size[1]/2 : w/2 + crop_size[1]/2]
+                #mask_t = mask_t[h/2 - crop_size[0]/2: h/2 + crop_size[0]/2][w/2 - crop_size[1]/2 : w/2 + crop_size[1]/2]
+                img_t = img[int(h/2 - crop_size[0]/2): int(h/2 + crop_size[0]/2)][int(w/2 - crop_size[1]/2) : int(w/2 + crop_size[1]/2)]
+                mask_t = mask[int(h/2 - crop_size[0]/2): int(h/2 + crop_size[0]/2)][int(w/2 - crop_size[1]/2) : int(w/2 + crop_size[1]/2)]
                 if(not os.path.exists("./crop/")):
-                    os.mkdir("./crop/images/")
-                    os.mkdir("./crop/masks/")
-                cv2.imwrite(f'./crop/images/{img_path[:-3]}_c.jpg', img_t)
-                cv2.imwrite(f'./crop/mask/{img_path[:-3]}_c.jpg', mask_t)
+                    os.makedirs("./crop/images/")
+                    os.makedirs("./crop/masks/")
+                cv2.imwrite(f'./crop/images/{img_path[:-4]}_c.jpg', img_t)
+                cv2.imwrite(f'./crop/masks/{img_path[:-4]}_c.jpg', mask_t)
+            elif(t=='inverse_gaussian'):
+                img_t = torchvision.transforms.functional.adjust_gamma(PIL.Image.fromarray(img),gamma=2,gain=1)
+                img_t = np.array(img_t)
+                if(not os.path.exists("./inverse_gaussian/")):
+                    os.makedirs("./inverse_gaussian/images/")
+                    os.makedirs("./inverse_gaussian/masks/")
+                cv2.imwrite(f'./inverse_gaussian/images/{img_path[:-4]}_ig.jpg', img_t)
+                cv2.imwrite(f'./inverse_gaussian/masks/{img_path[:-4]}_ig.jpg', mask)
 
-create_augmentations('NerveDataset/train/images', 'NerveDataset/train/ground_truth')
+            elif(t=='random_crop'):
+                h,w = img.shape
+                print(h, w)
+                center_x = torch.randint(low=(crop_size[0]//2) + 1, high=h-(crop_size[0]//2) - 1, size=(1,)).item()
+                center_y = torch.randint(low=(crop_size[1]//2) + 1, high=w-(crop_size[1]//2) - 1, size=(1,)).item()
+                img_t = img[int(center_x - crop_size[0]/2): int(center_x + crop_size[0]/2)][int(center_y - crop_size[1]/2) : int(center_y + crop_size[1]/2)]
+                mask_t = mask[int(center_x - crop_size[0]/2): int(center_x + crop_size[0]/2)][int(center_y - crop_size[1]/2) : int(center_y + crop_size[1]/2)]
+                if(not os.path.exists("./random_crop/")):
+                    os.makedirs("./random_crop/images/")
+                    os.makedirs("./random_crop/masks/")
+                cv2.imwrite(f'./random_crop/images/{img_path[:-4]}_rc.jpg', img_t)
+                cv2.imwrite(f'./random_crop/masks/{img_path[:-4]}_rc.jpg', mask_t)
+
+create_augmentations('MallesNet/train/images', 'MallesNet/train/ground_truth', (320, 336), ["inverse_gaussian"])
